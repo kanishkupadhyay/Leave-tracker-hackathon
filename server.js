62cbbc9fe7f175eaa123c6d60fd4ab9fe95dff7e
kanishk.upadhyay@mca <kanishk.upadhyay@mca>
2021-11-22 22:13:53 +0530

    remove express.static

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static())
 app.use(express.static(__dirname + '/public/index.html'));
 
 app.get('/*', function(req,res) {
